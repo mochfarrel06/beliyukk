fc3808472e265df1d272416acbca6d0b70ca6fe1
mochfarrel06 <mochfarrel2606@gmail.com>
2022-11-23 15:37:32 +0700

    Menambahkan page suggestion

--- src/components/other/SuggestionPage.tsx
+++ src/components/other/SuggestionPage.tsx
@@ -4,14 +4,15 @@ const SuggestionPage = () => {
   return (
     <>
       <div className="w-full mb-40">
-        <div className="container w-full mx-auto py-[60px] flex flex-col gap-[60px]">
+        <div className="container w-full mx-auto py-[60px] flex flex-col gap-10">
           <div className="text-[30px] font-medium font-reemkufi">Product suggestions</div>
           <div className="flex">
             <h4 className="text-c4 font-normal text-xl">Please provide suggestions or feedback about our products</h4>
-            <div>
-              <div className="w-[800px] h-[160px] border border-c4">
-                <input type="text" placeholder="Placeholder" />
+            <div className="flex flex-col gap-5">
+              <div className="w-[800px] h-[160px] border-[2px] border-c4 rounded-[10px] overflow-hidden">
+                <textarea placeholder="Placeholder" className="m-4 w-[760px] h-[120px] outline-none"></textarea>
               </div>
+              <button className="bg-c4 w-[103px] h-[52px] px-[26px] py-[14px] rounded-[10px] text-c6 font-medium text-base">Kirim</button>
             </div>
           </div>
         </div>
