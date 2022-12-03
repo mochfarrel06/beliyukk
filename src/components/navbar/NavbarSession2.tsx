a560dc56cbba497b055320e388ab697ce00036aa
mochfarrel06 <mochfarrel2606@gmail.com>
2022-12-03 14:03:01 +0700

    Memodifikasi navbar

--- src/components/navbar/NavbarSession2.tsx
+++ src/components/navbar/NavbarSession2.tsx
@@ -5,35 +5,41 @@ const NavbarSession2 = () => {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
     <>
-      <nav className="bg-c6 py-4 px-3 border border-c1">
-        <div className="container flex justify-between items-center">
-          <div className="w-full flex justify-between items-center">
-            <a href="#home" className="text-c1 text-sm font-semibold">
-              Beli<span className="text-c4">Yukk</span>
+      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-pink-500 mb-3">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white" href="#pablo">
+              BeliYukk
             </a>
-            <button onClick={() => setNavbarOpen(!navbarOpen)}>
-              <AiOutlineMenu className="text-xl text-c4" />
+            <button
+              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <AiOutlineMenu />
             </button>
           </div>
-          <div className={'lg:flex flex-grow items-center' + (navbarOpen ? ' flex' : ' hidden')}>
-            <div className="w-[460px] flex justify-between items-center">
-              <div>
-                <button className="text-base font-normal text-c4">Home</button>
-              </div>
-              <div>
-                <button className="text-base font-normal text-c4">Home</button>
-              </div>
-              <div>
-                <button className="text-base font-normal text-c4">Home</button>
-              </div>
-              <div>
-                <button className="text-base font-normal text-c4">Home</button>
-              </div>
-            </div>
-            <div className="w-36 flex items-center justify-between">
-              <button className="text-c4 text-lg font-medium">Login</button>
-              <button className="text-c1 text-lg font-medium">Login</button>
-            </div>
+          <div className={'lg:flex flex-grow items-center' + (navbarOpen ? ' flex' : ' hidden')} id="example-navbar-danger">
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+              <li className="nav-item">
+                <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+                  <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i>
+                  <span className="">Share</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+                  <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i>
+                  <span className="ml-2">Tweet</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+                  <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75"></i>
+                  <span className="ml-2">Pin</span>
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
       </nav>
